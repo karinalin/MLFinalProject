0d837ec2a485aa4e5d85c19d7fcb31583fe77bd5
Joyce Zhao <jzhao2@wellesley.edu>
2018-12-10 20:09:18 -0500

    more stuff

--- test.py
+++ test.py
@@ -1,29 +1,14 @@
+import os
 import numpy as np
-import re
-read = open("train-balanced-sarcasm-copy-copy.csv", "r")
-write = open("commas-fixed.csv", "w")
-
-for x in read:
-    split = x.split('"')
-    print(split)
-    
-    if (len(split) > 1):
-        split[0] = split[0].replace(",", "|")
-        split[2] = split[2].replace(",", "|")
-    else:
-        split[0] = split[0].replace(",", "|")
-        
-    if (len(split) == 3 or len(split) == 1):
-        write.write(''.join(split))
-
-write.close()
-read.close()
-
-
-
-DATA = np.genfromtxt("commas-fixed.csv", delimiter='|', skip_header=1, dtype='S')
-X = DATA[:,:1]
-y = DATA[:,1:]
-# print(X)
-print(y)
-# print(DATA)
+import pandas as pd
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+train_df = pd.read_csv('train-balanced-sarcasm.csv')
+train_df.dropna(subset=['comment'], inplace=True)
+wordcloud = WordCloud(background_color='white', stopwords = STOPWORDS,
+                max_words = 200, max_font_size = 100,
+                random_state = 17, width=800, height=400)
+plt.figure(figsize=(16, 12))
+wordcloud.generate(str(train_df.loc[train_df['label'] == 0, 'comment']))
+plt.imshow(wordcloud)
+plt.show()
